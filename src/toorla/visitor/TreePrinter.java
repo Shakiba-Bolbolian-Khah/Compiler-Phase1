479d6a9f760646ebdd15a473447baa9497baa658
hsazarmsa <hsazarmsa@gmail.com>
2019-04-05 18:22:46 +0430

    tree printer for statements

--- src/toorla/visitor/TreePrinter.java
+++ src/toorla/visitor/TreePrinter.java
@@ -18,65 +18,103 @@ import toorla.ast.statement.localVarStats.LocalVarDef;
 import toorla.ast.statement.localVarStats.LocalVarsDefinitions;
 import toorla.ast.statement.returnStatement.Return;
 
+import java.util.List;
+
 public class TreePrinter implements Visitor<Void> {
     //TODO : Implement all visit methods in TreePrinter to print AST as required in phase1 document
     @Override
     public Void visit(PrintLine printStat) {
+        Expression arg = printStat.getArg();
+        System.out.println("( print" + arg.accept(this) + ")");
         return null;
     }
 
     @Override
     public Void visit(Assign assignStat) {
+        Expression lvalue,rvalue;
+        lvalue = assignStat.getLvalue();
+        rvalue = assignStat.getRvalue();
+        System.out.println("( = " + lvalue.accept(this) + rvalue.accept(this) + ")");
         return null;
     }
 
     @Override
     public Void visit(Block block) {
+        List<Statement> body = block.body;
+        System.out.println("(");
+        for (int i = 0; i < body.size(); i++)
+            System.out.println(body.get(i).accept(this));
+        System.out.println(")");
         return null;
     }
 
     @Override
     public Void visit(Conditional conditional) {
+        System.out.println("( if");
+        System.out.println(conditional.getCondition().accept(this));
+        System.out.println(conditional.getThenStatement().accept(this));
+        System.out.println(conditional.getElseStatement().accept(this));
+        System.out.println(")");
         return null;
     }
 
     @Override
     public Void visit(While whileStat) {
+        System.out.println("( while ");
+        System.out.println(whileStat.expr.accept(this));
+        System.out.println(whileStat.body.accept(this));
+        System.out.println(")");
         return null;
     }
 
     @Override
     public Void visit(Return returnStat) {
+        System.out.println("( return");
+        System.out.println(returnStat.getReturnedExpr().accept(this));
+        System.out.println(")");
         return null;
     }
 
     @Override
     public Void visit(Break breakStat) {
+        System.out.println(breakStat.toString());
         return null;
     }
 
     @Override
     public Void visit(Continue continueStat) {
+        System.out.println(continueStat.toString());
         return null;
     }
 
     @Override
     public Void visit(Skip skip) {
+        System.out.println(skip.toString());
         return null;
     }
 
     @Override
     public Void visit(LocalVarDef localVarDef) {
+        System.out.println("( var ");
+        System.out.println(localVarDef.getLocalVarName().accept(this));
+        System.out.println(localVarDef.getInitialValue().accept(this));
+        System.out.println(")");
         return null;
     }
 
     @Override
     public Void visit(IncStatement incStatement) {
+        System.out.println("( ++");
+        System.out.println(incStatement.getOperand().accept(this));
+        System.out.println(")");
         return null;
     }
 
     @Override
     public Void visit(DecStatement decStatement) {
+        System.out.println("( ++");
+        System.out.println(decStatement.getOperand().accept(this));
+        System.out.println(")");
         return null;
     }
 
@@ -142,7 +180,7 @@ public class TreePrinter implements Visitor<Void> {
 
     @Override
     public Void visit(MethodCall methodCall) {
-        return null;
+        return null; 
     }
 
     @Override
@@ -196,9 +234,7 @@ public class TreePrinter implements Visitor<Void> {
     }
 
     @Override
-    public Void visit(ClassDeclaration classDeclaration) {
-        return null;
-    }
+    public Void visit(ClassDeclaration classDeclaration) { return null; }
 
     @Override
     public Void visit(EntryClassDeclaration entryClassDeclaration) {
